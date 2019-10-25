edc3e03a7a1b5a56a57370bd03828e2f70e4e6ae
Alex <bozeak@gmail.com>
2019-10-25 17:44:43 +0300

    EASYOPAC-1110 - Display times for same day repeatable events.

--- template.php
+++ template.php
@@ -74,3 +74,30 @@ function kulture_theme_process_page(&$vars) {
     _color_page_alter($vars);
   }
 }
+
+/**
+ * Returns HTML for a date element formatted as a range.
+ */
+function kulture_theme_date_display_range($variables) {
+  $date1 = $variables['date1'];
+  $date2 = $variables['date2'];
+  $timezone = $variables['timezone'];
+  $attributes_start = $variables['attributes_start'];
+  $attributes_end = $variables['attributes_end'];
+
+  $start_date = '<span class="date-display-start"' . drupal_attributes($attributes_start) . '>' . $date1 . '</span>';
+  $end_date = '<span class="date-display-end"' . drupal_attributes($attributes_end) . '>' . $date2 . $timezone . '</span>';
+
+  // If microdata attributes for the start date property have been passed in,
+  // add the microdata in meta tags.
+  if (!empty($variables['add_microdata'])) {
+    $start_date .= '<meta' . drupal_attributes($variables['microdata']['value']['#attributes']) . '/>';
+    $end_date .= '<meta' . drupal_attributes($variables['microdata']['value2']['#attributes']) . '/>';
+  }
+
+  // Wrap the result with the attributes.
+  return t('!start-date - !end-date', array(
+    '!start-date' => $start_date,
+    '!end-date' => $end_date,
+  ));
+}
