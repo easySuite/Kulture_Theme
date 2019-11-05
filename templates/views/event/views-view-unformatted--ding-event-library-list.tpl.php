267af80c5bbbed44192a8f0a2baf723e8c521395
Mihaela Stavila <ms@inleadmedia.com>
2019-11-05 09:51:59 +0200

    EASYOPAC-1115 - Added templates for views event and news on pg library.

---
+++ templates/views/event/views-view-unformatted--ding-event-library-list.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3 class="col-md-12 colums"><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="card col-md-4 ' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
+
